6d843a6a3dac9ca32af7d4262df4de055b05e840
harinarayan14 <gjayashankar2008@gmail.com>
2020-06-19 01:07:40 +0530

    piFinder

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function searchItUp() {
   if (index > 0) {
     indexP.html(index - 1 + " place in pi decimal digits");
   } else {
-    indexP.html('not found in the first 7.6 million digits');
+    indexP.html('not found in the first 14.1 million digits');
   }
 }
 
@@ -31,7 +31,7 @@ function setup() {
   Header.html("Find your digits in Pi");
   Header.position(displayWidth/2.4,displayHeight/25);
   Header1 = createElement("h2");
-  Header1.html("Pi finder (upto 7.6 million digits)");
+  Header1.html("Pi finder (upto 14.1 million digits)");
   Header1.position(displayWidth/2.5,displayHeight/10);
   digits = pi[0];
   searchBox = createInput('');
